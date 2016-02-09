1f7fcbfbc76d0b077181856b235511572ca08cd4
Robert Cunningham <rgc3dev@gmail.com>
2016-02-09 10:49:35 -0500

    Add paperclip, Add comments to recipe show page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -70,6 +70,6 @@ class RecipesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def recipe_params
-      params.require(:recipe).permit(:title, :author, :ingredients, :directions, :rating, :user_id)
+      params.require(:recipe).permit(:title, :author, :ingredients, :directions, :rating, :user_id, :avatar)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160209141037) do
+ActiveRecord::Schema.define(version: 20160209154050) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "author"
@@ -28,9 +28,13 @@ ActiveRecord::Schema.define(version: 20160209141037) do
     t.text     "ingredients"
     t.text     "directions"
     t.integer  "rating"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.integer  "user_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
